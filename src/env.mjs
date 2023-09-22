79506b565f2b3837cd00c17cd728e3f40d121c41
Rafal Golawski <rafal.j.golawski@gmail.com>
2023-09-22 14:50:53 +0200

    chore(env): add env variable to workflow

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,7 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: z.string().min(1),
     NEXT_PUBLIC_ERROR_REPORTING_URL: z.string().min(1),
     NEXT_PUBLIC_ERROR_REPORTING_APP_ID: z.string().min(1),
-    NEXT_PUBLIC_POCKETBASE_URL: z.string().min(1)
+    NEXT_PUBLIC_POCKETBASE_SENTINEL_URL: z.string().min(1)
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
@@ -15,7 +15,8 @@ export const env = createEnv({
       process.env.NEXT_PUBLIC_ERROR_REPORTING_URL,
     NEXT_PUBLIC_ERROR_REPORTING_APP_ID:
       process.env.NEXT_PUBLIC_ERROR_REPORTING_APP_ID,
-    NEXT_PUBLIC_POCKETBASE_URL: process.env.NEXT_PUBLIC_POCKETBASE_URL
+    NEXT_PUBLIC_POCKETBASE_SENTINEL_URL:
+      process.env.NEXT_PUBLIC_POCKETBASE_SENTINEL_URL
   },
   skipValidation: process.env.NODE_ENV === 'test'
 })
