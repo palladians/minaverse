a6b76f9f93b4b0c5897516e43162806140ba4916
Tomek Marciniak <tmarciniakm@gmail.com>
2023-10-18 10:02:19 +0200

    fix(code): fix linter issues

--- src/components/staking/staking-table.tsx
+++ src/components/staking/staking-table.tsx
@@ -254,9 +254,9 @@ export const StakingTable = ({
                         {header.isPlaceholder
                           ? null
                           : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
                       </TableHead>
                     )
                   })}
